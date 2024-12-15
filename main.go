499ae0795a555e1240746514273fca21e4b54f29
andreysemeno1 <andreysemeno1@mail.ru>
2024-12-15 12:53:06 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
 	"time"
 
-	tele ""
+	tele "gopkg.in/telebot.v4"
 )
 
 func main() {
@@ -16,31 +12,35 @@ func main() {
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 	})
 	if err != nil {
-		log.Fatal(err)
 		return
 	}
 
 	b.Handle("/start", func(c tele.Context) error {
-		_, _ = c.Send("Hello user!") // Ignore response from Send method to avoid errors if there's no connection to Telegram
-		return nil
+		return c.Send("Hello user!")package main
+
+import (
+	"time"
+
+	tele "gopkg.in/telebot.v4"
+)
+
+func main() {
+	b, err := tele.NewBot(tele.Settings{
+		Token:  "7518350168:AAHiWf8ACgkUZgVP7lvC8KRABaX9CabZyXQ",
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		return
+	}
+
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Hello user!")
 	})
 
 	b.Start()
+}
 
-	b.Handle("/stop", func(c tele.Context) error {
-		err := c.Send(b.Close())
-		if err != nil {
-			fmt.Println("Error sending /stop message:", err)
-		}
-		return nil
 	})
 
-	done := make(chan os.Signal)
-	signal.Notify(done, os.Interrupt)
-
-	select {
-	case s := <-done:
-		log.Println("Received signal", s)
-		b.Stop()
-	}
+	b.Start()
 }
