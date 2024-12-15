64971653e0812826848e869d154f4b6182824945
andreysemeno1 <andreysemeno1@mail.ru>
2024-12-15 13:25:03 +0300

    delete part main.go

--- main.go
+++ main.go
@@ -18,29 +18,3 @@ func main() {
 	b.Handle("/start", func(c tele.Context) error {
 		return c.Send("Hello user!")
 
-import (
-	"time"
-
-	tele "gopkg.in/telebot.v4"
-)
-
-func main() {
-	b, err := tele.NewBot(tele.Settings{
-		Token:  "7518350168:AAHiWf8ACgkUZgVP7lvC8KRABaX9CabZyXQ",
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-	})
-	if err != nil {
-		return
-	}
-
-	b.Handle("/start", func(c tele.Context) error {
-		return c.Send("Hello user!")
-	})
-
-	b.Start()
-}
-
-	})
-
-	b.Start()
-}
